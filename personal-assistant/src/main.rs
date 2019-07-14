a90d46f99c8381d3ec2c3a012766f20649bc20bb
oleksii <aleksey.petenko@gmail.com>
2019-07-14 15:14:50 +0300

    personal assistant project

---
+++ personal-assistant/src/main.rs
@@ -0,0 +1,73 @@
+use std::io::{stdin, stdout, Write};
+
+fn main() {
+    greet("Aid", "2019");
+    ask_name();
+    guess_age();
+    prove_count();
+    test_knowledge();
+    end();
+}
+
+fn greet(name: &str, year: &str) {
+    println!("Hello! My name is {}.", name);
+    println!("I was created in {}.", year);
+}
+
+fn ask_name() {
+    println!("Please, remind me your name.");
+    let name = read_line_with_prompt();
+    println!("What a great name you have, {}!", name);
+}
+
+fn guess_age() {
+    println!("Let me guess your age.");
+    println!("Enter remainders of dividing your age by 3, 5 and 7.");
+    let rem = read_line_with_prompt();
+    let vec = rem.split_whitespace()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let age = (vec[0] * 70 + vec[1] * 21 + vec[2] * 15) % 105;
+
+    println!("Your age is {}; that's a good time to start programming!", age);
+}
+
+fn test_knowledge() {
+    println!("Let's test your programming knowledge.");
+    println!("Why do we use methods?");
+    println!("1. To repeat a statement multiple times.");
+    println!("2. To decompose a program into several small subroutines.");
+    println!("3. To determine the execution time of a program.");
+    println!("4. To interrupt the execution of a program.");
+    loop {
+        let answer = read_line_with_prompt().parse::<i32>().unwrap();
+        if answer == 2 {
+            break;
+        } else {
+            println!("Please, try again.");
+        }
+    }
+}
+
+fn read_line_with_prompt() -> String {
+    print!(">");
+    stdout().flush().unwrap();
+    let mut name = String::new();
+    stdin().read_line(&mut name).unwrap();
+    name.trim().to_string()
+}
+
+fn prove_count() {
+    println!("Now I will prove to you that I can count to any number you want.");
+    let num = read_line_with_prompt().parse::<i32>().unwrap();
+    for i in 0..num {
+        println!("{}!", i);
+    }
+}
+
+fn end() {
+    println!("Congratulations, have a nice day!")
+}
+
+

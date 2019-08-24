17002e8ceadea12f6a48dd47e8efbccdc9dc7605
oleksii <aleksey.petenko@gmail.com>
2019-08-24 15:32:04 +0300

    encode decode Part 2 implementation

--- encode-decode/src/main.rs
+++ encode-decode/src/main.rs
@@ -1,11 +1,33 @@
+const ALPHABET_SIZE: u32 = 26;
+
 fn main() {
-    let message = "we found a treasure!";
-    for c in message.chars() {
-        let new_char = if c.is_alphabetic() {
-            'z' as u32 - (c as u32 - 'a' as u32)
-        } else {
-            c as u32
-        };
-        print!("{}", std::char::from_u32(new_char).unwrap());
+    let message = scan_stdin_line();
+    let key = scan_stdin_line().parse::<u32>().unwrap();
+    let encoded_messsage = encode(message, key);
+    println!("{}", encoded_messsage);
+}
+
+fn encode(msg: String, key: u32) -> String {
+    let mut encoded = String::new();
+    for c in msg.chars().into_iter() {
+        let new_char = shift(c, key);
+        encoded.push(new_char);
     }
+    encoded
+}
+
+fn shift(item: char, key: u32) -> char {
+    if !item.is_alphabetic() {
+        return item;
+    }
+    let starting_letter = 'a' as u32;
+    let actual_position = item as u32 - starting_letter;
+    let new_shift = (actual_position + key) % ALPHABET_SIZE;
+    std::char::from_u32(starting_letter + new_shift).unwrap()
+}
+
+fn scan_stdin_line() -> String {
+    let mut message = String::new();
+    std::io::stdin().read_line(&mut message).unwrap();
+    message.trim().to_string()
 }

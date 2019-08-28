cae79ffeb1179a2c7512bdbdcc7a7dbe1991b963
oleksii <aleksey.petenko@gmail.com>
2019-08-28 23:16:12 +0300

    linkedlist push pop and testing

--- algo-rust/src/linked_list_enum.rs
+++ algo-rust/src/linked_list_enum.rs
@@ -1,5 +1,33 @@
+use std::mem;
+
 pub struct List {
     head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List {
+            head: Link::Empty
+        }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        };
+        self.head = Link::Elem(Box::new(new_node));
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::Elem(nxt) => {
+                self.head = nxt.next;
+                Some(nxt.elem)
+            }
+        }
+    }
 }
 
 enum Link {
@@ -10,4 +38,35 @@ enum Link {
 struct Node {
     elem: i32,
     next: Link,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::linked_list_enum::{List, Node};
+
+    #[test]
+    fn verify_empty_pop() {
+        let mut xs = List::new();
+        assert_eq!(xs.pop(), None);
+    }
+
+    #[test]
+    fn verify_single_pop() {
+        let mut xs = List::new();
+        xs.push(1);
+        assert_eq!(xs.pop(), Some(1));
+        assert_eq!(xs.pop(), None);
+    }
+
+    #[test]
+    fn verify_multi_pop() {
+        let mut xs = List::new();
+        xs.push(1);
+        xs.push(2);
+        xs.push(3);
+        assert_eq!(xs.pop(), Some(3));
+        assert_eq!(xs.pop(), Some(2));
+        assert_eq!(xs.pop(), Some(1));
+        assert_eq!(xs.pop(), None);
+    }
 }
--- algo-rust/src/main.rs
+++ algo-rust/src/main.rs
@@ -1,4 +1,10 @@
+use algo_rust::linked_list_enum::List;
 
 fn main() {
     println!("Hello, world with algorithms and data structures!");
+    let mut xs = List::new();
+    xs.push(1);
+    xs.push(2);
+    println!("{:?}", xs.pop());
+    println!("{:?}", xs.pop());
 }
